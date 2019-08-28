9c192ae3d6fcd2451fdfd509c5bbdf9120248e4d
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2019-08-28 12:15:31 -0400

    Drop lodash dependency

--- test/index.test.js
+++ test/index.test.js
@@ -24,6 +24,22 @@ test('range', function(assert) {
             source: '<ul>{{#range 4}}<li>{{../foo}}{{this}}</li>{{/range}}</ul>',
             context: { foo: 'foo' },
             expected: '<ul><li>foo0</li><li>foo1</li><li>foo2</li><li>foo3</li></ul>',
+        },
+        {
+            source: '<ul>{{#range 0 -4 -1}}<li>{{this}}</li>{{/range}}</ul>',
+            expected: '<ul><li>0</li><li>-1</li><li>-2</li><li>-3</li></ul>',
+        },
+        {
+            source: '<ul>{{#range 1 4 0}}<li>{{this}}</li>{{/range}}</ul>',
+            expected: '<ul><li>1</li><li>1</li><li>1</li></ul>',
+        },
+        {
+            source: '<ul>{{#range 0}}<li>{{this}}</li>{{/range}}</ul>',
+            expected: '<ul></ul>',
+        },
+        {
+            source: '<ul>{{#range}}<li>{{this}}</li>{{/range}}</ul>',
+            expected: '<ul></ul>',
         }
     ];
 
